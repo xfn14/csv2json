28fff93b65095faa01cf4c62d8e29edc63e5324d
joaopsmendes <joao.ps.mendes2011@gmail.com>
2022-04-23 00:29:30 +0100

    define gramática

--- tradutor_lex.py
+++ tradutor_lex.py
@@ -6,14 +6,29 @@ literals = ['+', '-', '/', '*', '=', '(', ')']
 tokens = ['VAR', 'NUMBER']
 
 t_VAR = r'[a-zA-Z_][a-zA-Z0-9_]*'
-
+t_NUMBER = r'\d+'
 # def t_VAR(t):
 #     r'[a-zA-Z_][a-zA-Z0-9_]*' 
 
-def t_NUMBER(t):
-    r'\d+(\.\d+)?'
-    t.value = float(t.value)
-    return t
+# def t_NUMBER(t):
+#     r'\d+(\.\d+)?'
+#     # r'\d+'
+#     t.value = float(t.value)
+#     return t
+
+
+# # Define a rule so we can track line numbers
+# def t_newline(t):
+#     r'\n+'
+#     t.lexer.lineno += len(t.value)
+
+# # A string containing ignored characters (spaces and tabs)
+# t_ignore  = ' \t'
+
+# # Error handling rule
+# def t_error(t):
+#     print("Erro lexico no token '%s'" % t.value[0])
+#     t.lexer.skip(1)
 
 
 t_ignore = " \t\n"
