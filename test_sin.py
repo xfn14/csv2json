0e8b12482be2d76f15b681e7ac0f93045299d485
joaopsmendes <joao.ps.mendes2011@gmail.com>
2022-05-14 18:13:26 +0100

    alteraçao

--- test_sin.py
+++ test_sin.py
@@ -4,14 +4,6 @@ from test_lex import tokens, literals
 statementN = 0
 ts = {}
 
-def p_IdList_list(p):
-    "IdList : IdList ',' id"
-    p[0] = p[1] + [p[3]]
-
-def p_IdList_single(p):
-    "IdList : id"
-    p[0] = [p[1]]
-
 def p_Statement_Simples(p):
     "Statement : Expression"
     print(p[1])
@@ -53,11 +45,19 @@ def p_Expression_IGNORE_SINGLE(p):
     p[0] = 'ignore = ' + p[3] + '\n'
     
 def p_Expression_STAT(p):
-    "Expression : STAT : TEXT '{' Expression '}'"
+    "Expression : STAT ':' TEXT '{' Expression '}'"
     p[0] = 'def p_Statment_' + statementN + '(p):\n\t' + p[1] + ' : ' + p[3] + '\n\t' + p[5] + '\n\n'
     statementN += 1
 
 def p_Expression_EXP(p):
-    "Expression : EXP : TEXT '{' Expression '}'"
+    "Expression : EXP ':' TEXT '{' Expression '}'"
     p[0] = 'def p_Expressions_' + statementN + '(p):\n\t' + p[1] + ' : ' + p[3] + '\n\t' + p[5] + '\n\n'
     statementN += 1
+    
+def p_IdList_list(p):
+    "IdList : IdList ',' id"
+    p[0] = p[1] + [p[3]]
+
+def p_IdList_single(p):
+    "IdList : id"
+    p[0] = [p[1]]    
